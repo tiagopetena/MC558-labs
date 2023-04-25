d6c003ce349017d5925dd1933d55357d368d9006
Tiago Petena <tiagopetena@gmail.com>
2023-04-25 13:32:09 -0300

    added topological ordering

--- lab3-n-paths-1-goal/t3.py
+++ lab3-n-paths-1-goal/t3.py
@@ -14,7 +14,10 @@ class Graph(object):
     def __init__(self, n, m, s, t) -> None:
         self.n_nodes = n
         self.n_edges = m
-        self.adj_list = [[] for _ in range(0, n)]
+        adj_g = [[] for _ in range(0, n)]
+        adj_y = [[] for _ in range(0, n)]
+        adj_r = [[] for _ in range(0, n)]
+        self.adj_lists = [adj_g, adj_y, adj_r]
         self.in_degrees = [0 for _ in range(0, n)]
         self.out_degrees = [0 for _ in range(0, n)]
         self.edges = {}
@@ -31,14 +34,14 @@ class Graph(object):
 
     def link(self, a, b, color):
         edge = self.new_edge(a, b, color)
-        self.adj_list[a].append((b, edge.id))
+        self.adj_lists[color][a].append((b, edge.id))
 
     def set_color(self, u, color):
         self.colors[u] = color
 
     def __str__(self) -> str:
         adj_string = []
-        for i, node in enumerate(self.adj_list):
+        for i, node in enumerate(sum(self.adj_lists, [])):
             adjs = sorted([a[0] for a in node])
             node_str = f"{i}"
             if self.target == i:
@@ -46,12 +49,45 @@ class Graph(object):
             if self.start == i:
                 node_str += "(start)"
             adj_string.append(node_str + f": {' '.join(map(str, adjs))}")
-        return '\n'.join(adj_string)
+        return "\n".join(adj_string)
+
+
+def topological_ordering(G):
+    u = 0
+    stack = [G.start]
+    visited = [False for v in range(G.n_nodes)]
+    ordering = []
+    while stack != []:
+        visited_all = True
+        u = stack[-1]
+
+        visited_all = True
+        # Green
+        for v, edge_id in G.adj_lists[0][u]:
+            if visited[v] == False:
+                visited[v] = True
+                stack.append(v)
+                visited_all = False
+        # Yellow
+        for v, edge_id in G.adj_lists[1][u]:
+            if visited[v] == False:
+                visited[v] = True
+                stack.append(v)
+                visited_all = False
+        # Red
+        for v, edge_id in G.adj_lists[2][u]:
+            if visited[v] == False:
+                visited[v] = True
+                stack.append(v)
+                visited_all = False
+        if visited_all:
+            ordering.insert(0, u)
+            stack.pop()
+    print(ordering)
 
 
 def parse_input():
-
-    n, m, s, t= map(int, input().split(" "))
+    n, m, s, t = map(int, input().split(" "))
     G = Graph(n, m, s, t)
 
     for i in range(0, m):
@@ -64,6 +100,7 @@ def parse_input():
 def main():
     G = parse_input()
     print(G)
+    topological_ordering(G)
 
 
 if __name__ == "__main__":
