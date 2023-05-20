074963062ee8ed235c018fcf8ff33a918abc985d
Tiago Petena <tiagopetena@gmail.com>
2023-05-20 14:16:29 -0300

    add lab5

---
+++ lab5-adventure-world/README.md
@@ -0,0 +1,22 @@
+# Building clusters.
+Connect server clusters using the least amount of fiber cables.
+
+## Input
+
+1st line
+N: Number of computers($9 \le N \le 10^{4}$).
+M: Number of conections($N - 1 \le M \le 10^{5}$).
+K: Number of clusters($1 \le K \le N$).
+
+M lines:
+A: Computer 1.
+B: Computer 2 ($0 \le A,B \le N$).
+W: Conection cost($0 \le W \le 100$).
+
+## Goal
+
+Connect server clusters using the least amount of fiber cables.
+
+## Output
+
+1 number: total cost.
---
+++ lab5-adventure-world/t5.py
@@ -0,0 +1,67 @@
+# Lab5 - MC558 UNICAMP 1s2023
+# Tiago Petena RA187700
+
+
+class Edge(object):
+    def __init__(self, u, v, w) -> None:
+        self.nodes = (u, v)
+        self.weight = w
+        self.id = f"{self.nodes}"
+
+
+class Graph(object):
+    def __init__(self, n, m) -> None:
+        self.n_nodes = n
+        self.n_edges = m
+        self.adj_list = [[] for _ in range(0, n)]
+        self.in_degrees = [0 for _ in range(0, n)]
+        self.out_degrees = [0 for _ in range(0, n)]
+        self.edges = {}
+
+    def new_edge(self, u, v, w):
+        edge = Edge(u, v, w)
+        self.edges[edge.id] = edge
+        self.out_degrees[u] += 1
+        self.in_degrees[v] += 1
+        return edge
+
+    def link(self, a, b, w):
+        edge = self.new_edge(a, b, w)
+        self.adj_list[a].append((b, edge.id))
+        self.adj_list[b].append((a, edge.id))
+
+    def sort_edges_by_weight(self):
+        self.edges = {
+            k: v for k, v in sorted(self.edges.items(), key=lambda e: e[1].weight)
+        }
+        return self.edges
+
+    def __str__(self) -> str:
+        adj_string = []
+        for i, node in enumerate(self.adj_list):
+            adjs = sorted([a[0] for a in node])
+            node_str = f"{i}"
+            adj_string.append(node_str + f": {' '.join(map(str, adjs))}")
+        return "\n".join(adj_string)
+
+
+def parse_input():
+    n, m, k = map(int, input().split())
+    G = Graph(n, m)
+
+    for i in range(0, m):
+        u, v, w = map(int, input().split())
+        G.link(u, v, w)
+
+    return G, k
+
+
+def main():
+    G, k = parse_input()
+
+    total_cost = get_minimum_cost_kruskal(G, k)
+    print(total_cost)
+
+
+if __name__ == "__main__":
+    main()
